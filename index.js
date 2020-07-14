3b77172bb70502b8b0aff3d6f54d735b17c42228
yatoogamii <bracaval.elias.pro@gmail.com>
2020-07-15 00:08:24 +0200

    make it better

---
+++ index.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.once("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("message", msg => {
+  if (msg.content === "ping") {
+    msg.channel.send("Pong");
+  }
+});
+
+client.login(process.env.DiSCORD_TOKEN);
